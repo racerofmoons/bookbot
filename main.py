d98ed78942a8cd9bbbeebdd5b9f4c5c4260164d5
MoonRacer-GDT <jerothvotal@gmail.com>
2025-04-02 22:49:22 -0700

    added support for URLs (like gutenburg .txts)

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from stats import word_count, letter_count, sort_letters
+from stats import word_count, sort_letters
 import sys
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python main.py <filepath>")
+        print("Usage: python3 main.py <path or url>")
+        sys.exit(1)
+    try:
+        count = word_count(sys.argv[1])
+    except Exception as e:
+        print(f"Error: {e}")
         sys.exit(1)
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
-    print(f"Found {word_count(sys.argv[1])} total words")
+    print(f"Found {count} total words")
     print("--------- Character Count -------")
     for key, value in sort_letters(sys.argv[1]).items():
         print(f"{key}: {value}")
--- stats.py
+++ stats.py
@@ -1,6 +1,18 @@
+import requests
+import os
+
 def get_book_text(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return file.read() 
+    if filepath.startswith('http'):
+        response = requests.get(filepath)
+        if response.status_code == 200:
+            return response.text
+        else:
+            raise Exception(f"Failed to fetch the book from URL: {filepath}")
+    elif os.path.isfile(filepath):
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return file.read()
+    else:
+        raise Exception(f"Invalid path to file: {filepath}. Must be a valid URL or local file path.")
 
 def word_count(filepath):
     text = get_book_text(filepath)
